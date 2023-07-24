e4974956e67f936d9f6ad0ad4fc97907cb0d33d0
kameleonus <kamil10.00@o2.pl>
2023-07-24 17:44:05 +0200

    start

---
+++ src/main/java/pl/kamil/springAdvanced/AdvacedJava.java
@@ -0,0 +1,13 @@
+package pl.kamil.springAdvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvacedJava {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvacedJava.class,args);
+    }
+
+}
